959b56f19a0ca840d3954f420f2511edfa8bb586
Alexander <47358616+PanzerKnaker@users.noreply.github.com>
2026-03-27 14:23:06 +0300

    Add files via upload

---
+++ Warehouse/Microsoft/VisualBasic/Interaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Microsoft.VisualBasic
+{
+    internal class Interaction
+    {
+        internal static dynamic CreateObject(string v, string empty)
+        {
+            throw new NotImplementedException();
+            {
+
+            }
+        }
+    }
+}
